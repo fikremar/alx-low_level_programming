1ceae8e8f6a169e7f3f174f7eb9aa33994be0f1e
fikremar <tfikir143@gmail.com>
2023-03-20 06:14:58 -0700

    C first program message

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main- every code start from main function
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	/**
+	 * return- return zero if success
+	 */
+	return (0);
+}
