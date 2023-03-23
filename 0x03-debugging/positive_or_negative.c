88ca962fb69670908989a4699eb2cad750c003dc
fikremar <tfikir143@gmail.com>
2023-03-22 22:44:08 -0700

    debugging task 0

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,15 +1,15 @@
 #include"main.h"
 /**
- * postitive_or_negative - is function that evaluate number +ve or -ve
+ * positive_or_negative - is function that evaluate number +ve or -ve
  * values
  * @i: is parameter which is tobe evvaluasted
  */
-void postitive_or_negative(int i)
+void positive_or_negative(int i)
 {
 	if  (i > 0)
-		printf("%d is posetive number", i);
+		printf("%d is posetive number\n", i);
 	else if (i < 0)
-		printf("%d is negative nummber", i);
+		printf("%d is negative nummber\n", i);
 	else
-		printf("%d is Zero", i);
+		printf("%d is Zero\n", i);
 }
